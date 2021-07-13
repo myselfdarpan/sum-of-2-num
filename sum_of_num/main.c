5b1ef56b9909f9fb202dd9998e6c5cd39e8c69a1
DKS007 <darpankumarsingworld42@gmail.com>
2021-07-13 23:32:56 +0530

    initial commit

---
+++ sum_of_num/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sum,n1,n2;
+    printf("Enter the number:\n");
+    scanf("%d%d",&n1,&n2);
+    sum = n1+n2;
+    printf("The sum is %d", sum);
+}
